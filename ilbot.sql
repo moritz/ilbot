5ce5461a478b8a998071268a057d9dea59627ed5
moritz <moritz@c213334d-75ef-0310-aa23-eaa082d1ae64>
2007-06-04 23:47:26 +0000

    [irclog] updated ilbot.sql to generate the id column as well
    git-svn-id: http://svn.pugscode.org/pugs/misc/irclog@16633 c213334d-75ef-0310-aa23-eaa082d1ae64

--- ilbot.sql
+++ ilbot.sql
@@ -1,8 +1,10 @@
 DROP TABLE IF EXISTS `irclog`;
 CREATE TABLE `irclog` (
+        id INT auto_increment, 
         channel VARCHAR(30),
         day CHAR(10),
         nick VARCHAR(40),
         timestamp INT,
-        line TEXT
-        );
+        line TEXT,
+        PRIMARY KEY(`id`)
+        ) CHARSET=utf8;
