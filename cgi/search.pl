fe067d00107597433ef365fa13242745ce1a0d19
moritz <moritz@c213334d-75ef-0310-aa23-eaa082d1ae64>
2007-06-21 14:51:56 +0000

    [irclog] added paging to the search results, on agentzh++'s request
    git-svn-id: http://svn.pugscode.org/pugs/misc/irclog@16703 c213334d-75ef-0310-aa23-eaa082d1ae64

--- cgi/search.pl
+++ cgi/search.pl
@@ -10,16 +10,22 @@ use HTML::Entities;
 use HTML::Template;
 use IrcLog qw(get_dbh my_encode message_line);
 use Config::File;
+use List::Util qw(min);
 
 my $conf = Config::File::read_config_file("cgi.conf");
 my $base_url = $conf->{BASE_URL} || "/";
+my $days_per_page = 10;
+my $lines_per_day = 50; # not yet used
  
 my $q = new CGI;
 print "Content-Type: text/html; charset=UTF-8\n\n";
-my $t = HTML::Template->new(filename => "search.tmpl");
+my $t = HTML::Template->new(filename => "search.tmpl",
+		global_vars => 1);
 $t->param(BASE_URL => $base_url);
-my $lines_per_page = 500;
 my $start = $q->param("start") || 0;
+
+my $offset = $q->param("offset") || 0;
+die unless $offset =~ m/^\d+$/;
 
 my $dbh = get_dbh();
 {
@@ -28,6 +34,7 @@ my $dbh = get_dbh();
     my $q1 = $dbh->prepare("SELECT DISTINCT channel FROM irclog ORDER BY channel");
     $q1->execute();
     my $ch = $q->param('channel') || '';
+    $t->param(CURRENT_CHANNEL => $ch);
     while (my @row = $q1->fetchrow_array){
         if ($ch eq $row[0]){
             push @channels, {CHANNEL => $row[0], SELECTED => 1};
@@ -49,13 +56,27 @@ $t->param(NICK => $q->param('nick'));
 
 
 if (my $nick = $t->param('nick')){
-    # search for a nick, populate 'DAYS'
+    # search for a nick, populate 'DAYS' and result page links
     $nick = my_encode($nick);
 
     my $channel = my_encode($q->param('channel')) || die "No channel provided";
 
-    my $q1 = $dbh->prepare("SELECT DISTINCT day FROM irclog WHERE channel = ? AND ( nick = ? OR nick = ?) ORDER BY day DESC");
+    my $q0 = $dbh->prepare("SELECT COUNT(DISTINCT day) FROM irclog WHERE channel = ? AND (nick = ? OR nick = ?)");
+    my $q1 = $dbh->prepare("SELECT DISTINCT day FROM irclog WHERE channel = ? AND ( nick = ? OR nick = ?) ORDER BY day DESC LIMIT $days_per_page OFFSET $offset");
     my $q2 = $dbh->prepare("SELECT timestamp, line FROM irclog WHERE day = ? AND channel = ? AND (nick = ? OR nick = ?) ORDER BY id");
+
+    $q0->execute($channel, $nick, "* $nick");
+    my $result_count = ($q0->fetchrow_array);
+    $t->param(DAYS_COUNT => $result_count);
+    $t->param(DAYS_LOWER => $offset + 1);
+    $t->param(DAYS_UPPER => min($offset + $days_per_page, $result_count));
+
+    my @result_pages;
+    my $p = 1;
+    for (my $o = 0; $o <= $result_count; $o += $days_per_page){
+	    push @result_pages, { OFFSET => $o, PAGE => $p++ };
+    }
+    $t->param(RESULT_PAGES => \@result_pages);
 
     $q1->execute($channel, $nick, "* $nick");
     my $short_channel = $channel;
