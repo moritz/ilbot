1227eca1fd139878aea326f4b6263f619e6802ab
Aankhen <Aankhen@c213334d-75ef-0310-aa23-eaa082d1ae64>
2007-07-03 23:17:43 +0000

    * [irclog] forgot to check in IrcLog::WWW
    git-svn-id: http://svn.pugscode.org/pugs/misc/irclog@16819 c213334d-75ef-0310-aa23-eaa082d1ae64

---
+++ IrcLog/WWW.pm
@@ -0,0 +1,33 @@
+package IrcLog::WebUtil;
+use strict;
+use HTTP::Headers;
+
+require Exporter;
+our @ISA = qw(Exporter);
+our @EXPORT_OK = qw(http_header);
+
+sub http_header {
+    my $h = HTTP::Headers->new;
+    
+    $h->header(Status => '200 OK');
+    $h->header(Vary => 'Accept');
+    
+    my $accept = $ENV{HTTP_ACCEPT} || 'text/html';
+    
+    my %qs = (html => 1, xhtml => 0);
+    
+    if ($accept =~ m{ application/xhtml\+xml (; q= ([\d.]+) )? }x) {
+        $qs{xhtml} = ($2) ? $2 : 1;
+    }
+
+    if ($accept =~ m{ text/html (; q= ([\d.]+) )? }x) {
+        $qs{html} = ($2) ? $2 : 1;
+    }
+    
+    my $type = ($qs{xhtml} >= $qs{html}) ? 'application/xhtml+xml' : 'text/html';
+    $h->header('Content-Type' => "$type; charset=utf-8");
+    
+    return $h->as_string . "\n";
+}
+
+1;
