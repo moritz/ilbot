fd3462fb68b0b652f046b49460bb2ac26733da27
moritz <moritz@c213334d-75ef-0310-aa23-eaa082d1ae64>
2007-08-30 08:35:25 +0000

    [irclog] ilbot2.pl, a POCOIRC-based irc log bot works now
    git-svn-id: http://svn.pugscode.org/pugs/misc/irclog@17502 c213334d-75ef-0310-aa23-eaa082d1ae64

--- ilbot2.pl
+++ ilbot2.pl
@@ -4,46 +4,42 @@ use strict;
 
 # this is a cleaner reimplementation of ilbot, with Bot::BasicBot which 
 # in turn is based POE::* stuff
-
-use Config::File;
-use Bot::BasicBot;
-
 package IrcLogBot;
 use IrcLog qw(get_dbh gmt_today);
 {
 
     my $dbh = get_dbh();
 
-	sub prepare {
-		my $dbh = shift;
-		return $dbh->prepare("INSERT INTO irclog (channel, day, nick, timestamp, line) VALUES(?, ?, ?, ?, ?)");
-	}
-	my $q = prepare();
-	sub dbwrite {
-		my ($channel, $who, $line) = @_;
-		my @sql_args = ($channel, gmt_today(), $who, time, $line);
-		if ($dbh->ping){
-			$q->execute(@sql_args);
-		} else {
-			$q = prepare(get_dbh());
-			$q->execute(@sql_args);
-		}
-		return;
-	}
+    sub prepare {
+        my $dbh = shift;
+        return $dbh->prepare("INSERT INTO irclog (channel, day, nick, timestamp, line) VALUES(?, ?, ?, ?, ?)");
+    }
+    my $q = prepare($dbh);
+    sub dbwrite {
+        my ($channel, $who, $line) = @_;
+        my @sql_args = ($channel, gmt_today(), $who, time, $line);
+        if ($dbh->ping){
+            $q->execute(@sql_args);
+        } else {
+            $q = prepare(get_dbh());
+            $q->execute(@sql_args);
+        }
+        return;
+    }
 
     use base 'Bot::BasicBot';
 
     sub said {
         my $self = shift;
         my $e = shift;
-		dbwrite($e->{channel}, $e->{who}, $e->{body});
+        dbwrite($e->{channel}, $e->{who}, $e->{body});
         return undef;
     }
 
     sub emoted {
         my $self = shift;
         my $e = shift;
-		dbwrite($e->{channel}, '* ' . $e->{who}, $e->{body});
+        dbwrite($e->{channel}, '* ' . $e->{who}, $e->{body});
         return undef;
 
     }
@@ -51,14 +47,14 @@ use IrcLog qw(get_dbh gmt_today);
     sub chanjoin {
         my $self = shift;
         my $e = shift;
-		dbwrite($e->{channel}, '',  $e->{who} . ' joined ' . $e->{channel});
+        dbwrite($e->{channel}, '',  $e->{who} . ' joined ' . $e->{channel});
         return undef;
     }
 
     sub chanpart {
         my $self = shift;
         my $e = shift;
-		dbwrite($e->{channel}, '',  $e->{who} . ' left ' . $e->{channel});
+        dbwrite($e->{channel}, '',  $e->{who} . ' left ' . $e->{channel});
         return undef;
     }
 
@@ -71,10 +67,10 @@ use IrcLog qw(get_dbh gmt_today);
     }
 
     sub nick_change {
-		my ($self, $e) = @_;
-		print Dumper($e);
-		# XXX TODO
-		return undef;
+        my ($self, $e) = @_;
+        print Dumper($e);
+        # XXX TODO
+        return undef;
     }
 
     sub kicked {
@@ -85,22 +81,27 @@ use IrcLog qw(get_dbh gmt_today);
     }
 }
 
+use Config::File;
+use Bot::BasicBot;
+use Carp qw(confess);
+
+
 package main;
-my $conf = Config::File::read_config_file("bot.conf");
+my $conf = Config::File::read_config_file($ARGV[0] || "bot.conf");
 my $nick = shift @ARGV || $conf->{NICK} || "ilbot6";
 my $server = $conf->{SERVER} || "irc.freenode.org";
 my $port = $conf->{PORT} || 6667;
-my $channel = $conf->{CHANNEL} || "#perl6";
+my $channels = [ split m/\s+/, $conf->{CHANNEL}];
 
 my $bot = IrcLogBot->new(
-        server => $server,
-        port   => $port,
-        channels => [$channel],
+        server    => $server,
+        port      => $port,
+        channels  => [$channel],
         nick      => $nick,
         alt_nicks => ["irclogbot", "logbot"],
         username  => "bot",
-        name      => "irc log bot",
-        charset => "utf-8", 
+        name      => "irc log bot, http://moritz.faui2k3.org/en/ilbot",
+        charset   => "utf-8", 
         );
 $bot->run();
 
