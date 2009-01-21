4cc9d4ecdae38eca65419c2635abdb415979ba82
moritz <moritz@c213334d-75ef-0310-aa23-eaa082d1ae64>
2009-01-21 18:28:05 +0000

    [irclog] try to log /quit's via evil monkey-patching, rg++
    git-svn-id: http://svn.pugscode.org/pugs/misc/irclog@24989 c213334d-75ef-0310-aa23-eaa082d1ae64

--- ilbot2.pl
+++ ilbot2.pl
@@ -13,6 +13,79 @@ use Data::Dumper;
 
 {
 
+    # XXX since Bot::BasicBot doesn't provide an action for the
+    # quit message yet, we monkey-patch it in...
+    use POE::Kernel;
+    use POE::Session;
+    sub run {
+        my $self = shift;
+
+        # create the callbacks to the object states
+        POE::Session->create(
+            object_states => [
+                $self => {
+                    _start => "start_state",
+                    _stop  => "stop_state",
+
+                    irc_001          => "irc_001_state",
+                    irc_msg          => "irc_said_state",
+                    irc_public       => "irc_said_state",
+                    irc_ctcp_action  => "irc_emoted_state",
+                    irc_ping         => "irc_ping_state",
+                    reconnect        => "reconnect",
+
+                    irc_disconnected => "irc_disconnected_state",
+                    irc_error        => "irc_error_state",
+
+                    irc_join         => "irc_chanjoin_state",
+                    irc_part         => "irc_chanpart_state",
+                    irc_kick         => "irc_kicked_state",
+                    irc_nick         => "irc_nick_state",
+                    irc_mode         => "irc_mode_state",
+                    irc_quit         => "irc_quit_state",
+
+                    fork_close       => "fork_close_state",
+                    fork_error       => "fork_error_state",
+
+                    irc_353          => "names_state",
+                    irc_366          => "names_done_state",
+
+                    irc_332          => "topic_raw_state",
+                    irc_topic        => "topic_state",
+
+                    irc_391          => "_time_state",
+                    _get_time        => "_get_time_state",
+                    
+                    tick => "tick_state",
+                }
+            ]
+        );
+
+        # and say that we want to recive said messages
+        $poe_kernel->post( $self->{IRCNAME} => register => 'all' );
+
+        # run
+        $poe_kernel->run() unless $self->{no_run};
+    }
+
+    sub irc_quit_state {
+        my ($self, $kernel, $nick, $message) = @_[OBJECT, KERNEL, ARG0, ARG1];
+        $nick = $_[OBJECT]->nick_strip($nick);
+        if ($self->nick eq $nick) {
+            $kernel->delay('reconnect', 1 );
+            return;
+        }
+        for my $channel (keys( %{ $self->{channel_data} } )) {
+            if (defined($self->{channel_data}{$channel}{$nick})) {
+                $_[OBJECT]->_remove_from_channel( $channel, $nick );
+                @_[ARG1, ARG2] = ($channel, $message);
+                irc_chan_received_state( 'chanpart', 'say', @_ );
+            }
+        }
+    }
+    # end of monkeypatching
+
+
     my $dbh = get_dbh();
 
     sub prepare {
@@ -56,6 +129,13 @@ use Data::Dumper;
         my $self = shift;
         my $e = shift;
         dbwrite($e->{channel}, '',  $e->{who} . ' joined ' . $e->{channel});
+        return undef;
+    }
+
+    sub chanquit {
+        my $self = shift;
+        my $e = shift;
+        dbwrite($e->{channel}, '', $e->{who} . ' left ' . $e->{channel});
         return undef;
     }
 
