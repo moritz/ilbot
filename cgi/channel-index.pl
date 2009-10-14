52553ffbaf23974c12d1296bd72b169b34f78b54
geoffb <geoffb@c213334d-75ef-0310-aa23-eaa082d1ae64>
2009-10-14 07:14:54 +0000

    [irclog] cgi/channel-index.pl: Redesign calendar HTML generation
    git-svn-id: http://svn.pugscode.org/pugs/misc/irclog@28784 c213334d-75ef-0310-aa23-eaa082d1ae64

--- cgi/channel-index.pl
+++ cgi/channel-index.pl
@@ -1,95 +1,115 @@
 #!/usr/bin/perl
-use warnings;
 use strict;
-use Date::Simple qw(date);
+use warnings;
+use Calendar::Simple;
 use CGI::Carp qw(fatalsToBrowser);
 use CGI;
-use HTML::Entities;
-use HTML::Template;
 use Config::File;
-use HTML::Calendar::Simple;
+use HTML::Template;
 use Cache::FileCache;
 use lib 'lib';
 use IrcLog qw(get_dbh gmt_today);
 
-my $q = new CGI;
-my $channel = $q->url_param('channel');
-print "Content-Type: text/html; charset=utf-8\n\n";
+# test_calendar();
+go();
 
-my $cache_name = $channel . '|' . gmt_today();
+sub go {
+    my $q = new CGI;
+    my $channel = $q->url_param('channel');
+    print "Content-Type: text/html; charset=utf-8\n\n";
 
-my $cache = new Cache::FileCache( { 
-		namespace 		=> 'irclog',
-		} );
-my $data = $cache->get($cache_name);
-if (! defined $data){
-    $data = get_channel_index();
-    $cache->set($data, '2 hours');
+    my $cache_name = $channel . '|' . gmt_today();
+    my $cache      = new Cache::FileCache({ namespace => 'irclog' });
+    my $data       = $cache->get($cache_name);
+
+    if (! defined $data) {
+        $data = get_channel_index($channel);
+        $cache->set($data, '2 hours');
+    }
+
+    print $data;
 }
-print $data;
+
+sub test_calendar {
+    my $channel  = '#parrotsketch';
+    my $base_url = '/irclog/';
+    my $dates    = [qw( 2009-09-28 2009-09-30
+                        2009-10-01 2009-10-02 2009-10-05 2009-10-12 )];
+
+    print calendar_for_channel($channel, $dates, $base_url);
+}
 
 sub get_channel_index {
-
-    my $conf = Config::File::read_config_file('cgi.conf');
+    my $channel  = shift;
+    my $conf     = Config::File::read_config_file('cgi.conf');
     my $base_url = $conf->{BASE_URL} || q{/};
+
     my $t = HTML::Template->new(
             filename            => 'template/channel-index.tmpl',
             die_on_bad_params   => 0,
     );
 
-    my $dbh = get_dbh();
-
-    # we are evil and create a calendar entry for month between the first 
+    # we are evil and create a calendar entry for month between the first
     # and last date
-    my $q3 = $dbh->prepare('SELECT DISTINCT day FROM irclog WHERE channel = ?');
-
+    my $dbh       = get_dbh();
+    my $get_dates = 'SELECT DISTINCT day FROM irclog WHERE channel = ? ORDER BY day';
+    my $dates     = $dbh->selectcol_arrayref($get_dates, undef, $channel);
 
     $t->param(CHANNEL  => $channel);
     $t->param(BASE_URL => $base_url);
-    $t->param(CALENDAR => calendar_for_channel($channel, $q3, $base_url)); 
+    $t->param(CALENDAR => calendar_for_channel($channel, $dates, $base_url));
     return $t->output;
 }
 
 sub calendar_for_channel {
-    my ($channel, $query, $base_url)  = @_;
+    my ($channel, $dates, $base_url)  = @_;
     $channel =~ s/\A\#//smx;
-    $query->execute('#' . $channel);
-    my %cals;
-    while (my ($day) = $query->fetchrow_array){
-        # extract year and month part: (YYYY-MM)
-        my $key = substr $day, 0, 7;
-        # day
-        my $d = substr $day, 8;
 
-        # create calendar
-        if (not exists $cals{$key}){
-            my ($year, $month) = split m/-/smx, $key;
-            $cals{$key} = HTML::Calendar::Simple->new({
-                    year  => $year,
-                    month => $month,
-                    });
+    my (%months, %link);
+    for my $date (@$dates) {
+        my ($Y, $M, $D) = split '-', $date;
+        $link{$date}    = "$base_url$channel/$D";
+        $months{"$Y-$M"}++;
+    }
+
+    my @months  = qw( Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec );
+    my @days    = qw( S M T W T F S );
+    my $dayhead = join '' => map "<th>$_</th>" => @days;
+    my $html    = qq{<div class="calendars">\n};
+
+    my %cals;
+    for my $month (reverse sort keys %months) {
+        my  ($Y, $M) = split '-', $month;
+        my   $title  = $months[$M - 1] . ' ' . $Y;
+        my   @weeks  = calendar($M, $Y);
+        push @weeks, [] while @weeks < 6;
+
+        $html .= qq{<table class="calendar">\n<thead>\n<tr class="calendar_title"><th colspan="7">$title</th></tr>\n<tr class="day_names">$dayhead</tr>\n</thead>\n<tbody>\n};
+
+        for my $week (@weeks) {
+            $html .= qq{<tr>};
+
+            for my $day_num (0 .. 6) {
+                my $day     = $week->[$day_num];
+                my $content = '';
+
+                if ($day) {
+                    my $D    = sprintf '%02d', $day;
+                    my $link = $link{"$Y-$M-$D"};
+                    $content = $link ? qq{<a href="$link">$day</a>}
+                                     : $day;
+                }
+
+                $html .= qq{<td>$content</td>};
+            }
+
+            $html .= qq{</tr>\n};
         }
 
-        # populate calendar with links
-        $cals{$key}->daily_info({
-                day      => $d,
-                day_link => "$base_url$channel/$day",
-                });
+        $html .= qq{</tbody>\n</table>\n};
     }
 
-    # now generate the HTML output
-    my $html = q{};
-    my $sorter = sub {
-        my ($l, $r) = @_;
-        return 12 * $cals{$l}->year + $cals{$l}->month
-            <=> 12 * $cals{$r}->year + $cals{$r}->month;
-    };
-
-    for my $cal (reverse sort { &$sorter($a, $b) } keys %cals){
-        $html .= qq{\n<div class="calendar">}
-            . $cals{$cal}->calendar_month
-            . qq{</div>\n}
-    }
+    $html .= qq{</div>\n};
 
     return $html;
 }
