ab12c3263275ad3c7ee00465ed36b332fb3f05fa
Moritz Lenz <moritz@faui2k3.org>
2013-06-09 18:08:18 +0200

    combine 3 JS files into one
    should reduce the HTTP overhead a wee bit

--- static/s/dynamic.js
+++ static/s/dynamic.js
@@ -1,3 +1,46 @@
+// collapse mulitple joins/quits into one line
+$(document).ready(function() {
+    var hash = window.location.hash;
+    var uncollapse;
+
+    $('tr.special.new').each(function (idx, elem) {
+        var ids = [$(elem).attr('id')];
+        $(elem).nextAll().each(function (i, e) {
+            if ($(e).hasClass('special')) {
+                $(e).addClass('hidden');
+                if (hash && $(e).find(hash).length) {
+		    uncollapse = ids[0];
+                }
+                ids.push($(e).attr('id'));
+            }
+            else {
+                /* abort iteration */
+                return false;
+            }
+
+        });
+        var c = ids.length;
+        if (c > 1) {
+            $(elem).data('ids', ids);
+            $(elem).addClass('hidden');
+            var extra_class = 'light';
+            if ($(elem).hasClass('dark')) {
+                extra_class = 'dark';
+            }
+            $(elem).before('<tr class="special ' + extra_class + '"><td class="nick" /> <td /><td>' + c + ' more elements. <a href="javascript:show_collapsed(\'' + $(elem).attr('id') + '\')">Show/hide.</a></td></tr>');
+	   if (uncollapse) {
+                show_collapsed(uncollapse);
+           }
+        }
+    });
+});
+
+function show_collapsed(id) {
+    var ids = $('#' + id).data('ids');
+    for (i in ids ) {
+        $('#' + ids[i]).toggleClass('hidden');
+    }
+}
 /*
 Maybe-helpful filtering-by-nick stuff. Lets you filter nicks in/out, thus
 letting you view particular conversations. Sorta.
@@ -272,4 +315,83 @@ function filtering_off() {
 
     $("#" + filter_toggle_id + ">a").html("Turn on filtering by nick");
     $("#" + filter_toggle_id + ">a").attr("href", 'javascript:filtering_on()');
+}
+
+// Summary features
+var summary_filter_link = '<a href="javascript:hide_non_summary()">show only summary lines</a>';
+var enable_summary_mode_html = ' <a href="javascript:enable_summary_mode()">Enable summary mode</a>';
+var summary_mode_html = '<a href="javascript:save_summary_changes()">Save summary changes</a>, <span id="toggle_summary">' + summary_filter_link + '</span> <a href="javascript:disable_summary_mode()">disable summary mode</a>';
+$(document).ready(function() {
+    disable_summary_mode();
+});
+
+function enable_summary_mode() {
+    if ($('.summary').length) {
+        $('.summary').show();
+        $('#summary_container').html(summary_mode_html);
+        return;
+    }
+    $('#summary_container').html('Loading...');
+    var url = IlbotConfig.base_url + 'e/' + IlbotConfig.channel + '/' + IlbotConfig.day + '/summary';
+    $.ajax(url, {
+        accept: 'application/json',
+        success: function(d) {
+            $('#log th').eq(0).after('<th class="summary">S</th>');
+            $('#log tr').each(function (idx, e) {
+                var id = $(e).find('.time').attr('id');
+                if (id) {
+                    var i = id.substr(2);
+                    var to_insert = '<td class="summary"><input type="checkbox" name="in_summary_' + i + '" class="summary_checkbox" /></td>';
+                    $(e).find('.nick').before(to_insert);
+                }
+                else {
+                    $(e).find('.nick').before('<td class="summary" />');
+                }
+
+            });
+            for (var checked_idx in d) {
+                var selector = '#i_' + d[checked_idx];
+                $(selector).parent().find('input').attr('checked', 'checked').addClass('originally_checked');
+            }
+            $('#summary_container').html(summary_mode_html);
+        }
+    });
+}
+function disable_summary_mode() {
+    $('.summary').hide();
+    $('#summary_container').html(enable_summary_mode_html);
+}
+
+function save_summary_changes() {
+    var newly_checked = [];
+    var was_checked = [];
+    $('.originally_checked').each(function(index, element) {
+            if (! $(element).is(':checked')) {
+                was_checked.push(
+                    $(element).attr('name').split('_')[2]
+                );
+            }
+    });
+    $('.summary_checkbox:checked').each(function(index, element) {
+            if (! $(element).attr('class').match(/originally_checked/)) {
+                newly_checked.push(
+                    $(element).attr('name').split('_')[2]
+                );
+            }
+    });
+    if (was_checked.length != 0 || newly_checked.length != 0) {
+        $.post(IlbotConfig.base_url +  "e/summary", { check: newly_checked.join('.'), uncheck: was_checked.join('.') } );
+    }
+}
+
+function hide_non_summary() {
+    $('.summary_checkbox:not(:checked)').parents('tr').hide();
+    $('#toggle_summary').html('<a href="javascript:show_all_rows()">show all lines</a>');
+    $('tr.cont td.nick').css('visibility', 'visible');
+}
+
+function show_all_rows() {
+    $('.summary_checkbox').parents('tr').show();
+    $('#toggle_summary').html(summary_filter_link);
+    $('tr.cont td.nick').css('visibility', 'hidden');
 }
