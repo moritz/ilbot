a7160a3a32f0788b25b8f5273a6f7cf6a457da6a
Moritz Lenz <moritz@faui2k3.org>
2013-06-18 21:49:10 +0200

    [search] fix schema; add some progress indicators

--- lib/Ilbot/Backend/Search.pm
+++ lib/Ilbot/Backend/Search.pm
@@ -1,6 +1,7 @@
 package Ilbot::Backend::Search;
 use strict;
 use warnings;
+use 5.010;
 
 use Lucy::Index::Indexer;
 use Lucy::Plan::Schema;
@@ -25,6 +26,8 @@ sub backend { $_[0]->{backend} };
 sub indexer {
     my ($self, %opt) = @_;
     die 'Missing argument "channel"' unless $opt{channel};
+
+    $| = 1;
     my $channel = $opt{channel};
     $channel =~ tr/a-zA-Z0-9_-//cd;
     # Create a Schema which defines index fields.
@@ -35,11 +38,12 @@ sub indexer {
     my $type = Lucy::Plan::FullTextType->new(
         analyzer => $polyanalyzer,
     );
-    $schema->spec_field( name => 'id',      type => Lucy::Plan::Int64Type->new() );
-    $schema->spec_field( name => 'day',     type => Lucy::Plan::StringType->new);
+    $schema->spec_field( name => 'id',      type => Lucy::Plan::Int64Type->new( indexed => 0, sortable => 1) );
+    $schema->spec_field( name => 'day',     type => Lucy::Plan::StringType->new( indexed => 0, sortable => 1));
     $schema->spec_field( name => 'nick',    type => $type );
     $schema->spec_field( name => 'line',    type => $type );
 
+    say "idx-path: ", join('/', config('config_root'), '../search-idx', $channel);
     my $indexer = Lucy::Index::Indexer->new(
         schema => $schema,
         index  => join('/', config('config_root'), '../search-idx', $channel),
@@ -53,9 +57,13 @@ sub index_all {
     for my $channel (@{ $self->backend->channels }) {
         my $b = $self->backend->channel(channel => $channel);
         my $i = $self->indexer(channel => $channel);
+        say '';
+        say $channel;
         for my $d (@{ $b->days_and_activity_counts }) {
             my $day = $d->[0];
+            print "\r$day";
             for my $line (@{ $b->lines(day => $day) }) {
+                next unless defined $line->[1];
                 $i->add_doc({
                     day     => $day,
                     id      => $line->[0],
